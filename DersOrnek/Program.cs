1aac465d3b846ff2ab35cc6d74654e00f62328fd
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2022-03-08 00:27:09 +0300

    Add project files.

---
+++ DersOrnek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+            
+        }
+    }
+}
+
+/*
+ int n;
+int[] array = new int[n];
+
+int newArray(int array, int k);
+{
+    int gecicic = 0;
+
+    for(int i=0; i < n; i++)
+    {
+
+
+    }
+
+    return  ;
+}
+*/
+//  Kenarları A, B, C, D olarak verilen bir dörtgenin kare olup olmadığını bulan programın C++ / Java programlama dilini kullanarak yazınız. 
+
+
+ 
+
+
+
+   
